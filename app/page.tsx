8f151aff1960dcebda76ee719ea30497cddfe027
Om453 <om.patel.cs21@ggits.net>
2024-05-21 20:01:01 +0530

    change text in inner components

--- app/page.tsx
+++ app/page.tsx
@@ -389,8 +389,8 @@ const HomePage = (props: Props) => {
           <strong>Camera Feed Highlighting 🎨</strong>
           <p>
             Highlights persons in{" "}
-            <span style={{ color: "#FF0F0F" }}>red</span> and other objects in{" "}
-            <span style={{ color: "#00B612" }}>green</span>.
+            <span style={{ color: "#FF0F0F" }}>green</span> and other objects in{" "}
+            <span style={{ color: "#00B612" }}>red</span>.
           </p>
         </li>
         <Separator />
