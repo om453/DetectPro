92f57c8ed2d4cc394f7745eb5ffecee5955347c1
Om453 <om.patel.cs21@ggits.net>
2024-05-23 21:09:54 +0530

    favicon added

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,13 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link
-          rel="icon"
-          href="/icon?<generated>"
-          type="image/<generated>"
-          sizes="<generated>"
-        />
+      <link rel="icon" href="/favicon.ico" sizes="any" />
       </head>
       <body className={inter.className}>
         <ThemeProvider
